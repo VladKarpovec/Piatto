1779b259635ce679e8e7716c2b96033cf2a1e6c0
VladKarpovec <156462598+VladKarpovec@users.noreply.github.com>
2025-11-12 18:58:43 +0200

    Update migration dependency to initial menu migration

--- reviews/migrations/0001_initial.py
+++ reviews/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('menu', '0002_remove_dish_stock'),
+        ('menu', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
