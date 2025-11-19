ee178dc1d9ad24f9b8dbef57a65e0f724b70fa27
Vlad <vladkarpovec44@gmail.com>
2025-11-19 19:37:50 +0200

    remake reviews

--- reviews/models.py
+++ reviews/models.py
@@ -7,7 +7,7 @@ from menu.models import Dish
 class Review(models.Model):
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE, related_name="reviews")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.PositiveIntegerField(default=5, choices=[(i, i) for i in range(1, 5)])
+    rating = models.PositiveIntegerField(default=5, choices=[(i, i) for i in range(1, 6)])
     comment = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
